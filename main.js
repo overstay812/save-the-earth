5f93aada386409c907ede85874857a1a7746c910
Konstantin Surnin <overstay812@gmail.com>
2020-09-27 13:25:19 +0300

    create common styles

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+let moveBg = (event) => {
+    const bg = document.querySelector('.bg')
+    const bird = document.querySelector('.bird')
+    const content = document.querySelector('.content')
+
+
+    bg.style.width = 100 + event.pageX / 100 + '%'
+    bg.style.height = 100 + event.pageX / 100 + '%'
+
+    bird.style.right = 100 + event.pageX / 2 + 'px'
+
+    content.style.left = 100 + event.pageX / 2.5 + 'px'
+
+}
+
+document.addEventListener('mouseover', moveBg)
